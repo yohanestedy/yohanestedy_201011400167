40321f03c0d22acbfffbc3a503e8c43dbb406bbe
Yohanes Tedy <yohanestedy52b@gmail.com>
2023-03-14 00:21:41 +0700

    update

--- app/src/main/java/com/ini_package_name/yohanestedy/MainActivity.java
+++ app/src/main/java/com/ini_package_name/yohanestedy/MainActivity.java
@@ -2,8 +2,12 @@ package com.ini_package_name.yohanestedy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,10 +20,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvUserId = findViewById(R.id.tv_user_id);
+        Button btnExit = findViewById(R.id.btn_exit);
 
         Intent intent = getIntent();
         String userId = intent.getStringExtra("USER_ID");
 
         tvUserId.setText("Selamat Datang: " + userId);
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Exit");
+                builder.setMessage("Yakin mau keluar nih?");
+                builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Exit the app
+                        finishAffinity();
+                    }
+                });
+                builder.setNegativeButton("Tidak", null);
+                builder.show();
+            }
+        });
     }
+
+
 }
